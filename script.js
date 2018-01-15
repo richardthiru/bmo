deb888524e749056e927c6929b1ff5bcf0b40808
richardthiru <rtthiru21@gmail.com>
2018-01-15 12:28:26 -0500

    Add files via upload
    all files

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidenav").style.display = "block";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("sidenav").style.display = "none";
+}
+
